695bcfbfc21a9c547c8b8986c2e6286f48ab70b9
deekshith4 <deekshith@striketru.com>
2026-02-19 20:02:55 +0530

    Initial commit

---
+++ src/main/java/com/pipeline/pipeline/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.pipeline.pipeline.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Application is working!";
+    }
+}
