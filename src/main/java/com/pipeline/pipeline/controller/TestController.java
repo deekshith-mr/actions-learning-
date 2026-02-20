1098d6862390e83144b3feee69a95eaf99a09021
deekshith4 <deekshith@striketru.com>
2026-02-20 20:47:04 +0530

    Test

--- src/main/java/com/pipeline/pipeline/controller/TestController.java
+++ src/main/java/com/pipeline/pipeline/controller/TestController.java
@@ -8,6 +8,7 @@ public class TestController {
 
     @GetMapping("/test")
     public String test() {
+        System.out.println("Testing...");
         return "Application is working!";
     }
     @GetMapping("/test2")
